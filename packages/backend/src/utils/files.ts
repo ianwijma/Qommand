8fff8e09561f9b47317d3d6740f14a4c85692a36
ianwijma <ian@wij.ma>
2025-02-02 20:00:48 +1100

    Added file ops

--- packages/backend/src/utils/files.ts
+++ packages/backend/src/utils/files.ts
@@ -21,18 +21,31 @@ const normalizeFilePath = (relativePath: string): string => {
     return normalizePath;
 }
 
-export const ensureDirExists = async (relativeFilePath: string): Promise<void> => {
+export const ensureDirExists = async (relativeFilePath: string): Promise<string> => {
     const normalizePath = normalizeFilePath(relativeFilePath);
 
     const dir = path.dirname(normalizePath);
 
     await fs.mkdir(dir, {recursive: true});
+
+    return normalizePath;
 }
 
 export const readFile = async (relativeFilePath: string): Promise<string> => {
     const normalizePath = normalizeFilePath(relativeFilePath);
 
     return await fs.readFile(normalizePath, 'utf8');
+}
+
+export const ensureFileExists = async (relativeFilePath: string, defaultContent: string): Promise<string> => {
+    const normalizePath = normalizeFilePath(relativeFilePath);
+
+    const foundFile = await fileExists(normalizePath);
+    if (!foundFile) {
+        await writeFile(normalizePath, defaultContent);
+    }
+
+    return normalizePath;
 }
 
 export const writeFile = async (relativeFilePath: string, content: string): Promise<void> => {
@@ -61,8 +74,32 @@ export const readYamlFile = async <T>(relativeFilePath: string): Promise<T> => {
     return YAML.parse(fileContent) as T;
 }
 
-export const writeYamlFile = async <T>(relativeFilePath: string, context: T): Promise<void> => {
-    const fileContent = YAML.stringify(context);
+export const writeYamlFile = async <T>(relativeFilePath: string, content: T): Promise<void> => {
+    const fileContent = YAML.stringify(content);
 
     await writeFile(relativeFilePath, fileContent);
+}
+
+export const ensureYamlFileExists = async <T>(relativeFilePath: string, defaultContent: T): Promise<string> => {
+    const fileContent = YAML.stringify(defaultContent);
+
+    return await ensureFileExists(relativeFilePath, fileContent);
+}
+
+export const readJsonFile = async <T>(relativeFilePath: string): Promise<T> => {
+    const fileContent = await readFile(relativeFilePath);
+
+    return JSON.parse(fileContent) as T;
+}
+
+export const writeJsonFile = async <T>(relativeFilePath: string, context: T): Promise<void> => {
+    const fileContent = JSON.stringify(context);
+
+    await writeFile(relativeFilePath, fileContent);
+}
+
+export const ensureJsonFileExists = async <T>(relativeFilePath: string, defaultContent: T): Promise<string> => {
+    const fileContent = JSON.stringify(defaultContent);
+
+    return await ensureFileExists(relativeFilePath, fileContent);
 }
