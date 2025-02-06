f1ba26960cc12b31797d7acb4a99e1f84845304e
ianwijma <ian@wij.ma>
2025-02-06 21:14:32 +1100

    Initial changes

--- packages/backend/vite.main.config.ts
+++ packages/backend/vite.main.config.ts
@@ -1,14 +1,4 @@
 import {defineConfig} from 'vite';
-import native from 'vite-plugin-native'
 
 // https://vitejs.dev/config
-export default defineConfig({
-    plugins: [
-        native({
-            webpack: {
-                "node-loader": {},
-                "@vercel/webpack-asset-relocator-loader": {}
-            }
-        })
-    ]
-});
+export default defineConfig({});
